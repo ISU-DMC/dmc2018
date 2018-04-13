fda6f90b67fcc6f39638540f66ed5f06684bb791
kgulzina <gkuttubekova@gmail.com>
2018-04-12 21:38:19 -0500

    some findings on var:color

---
+++ users/kgulzina/dmc_1.R
@@ -0,0 +1,104 @@
+library(ggplot2)
+library(caret)
+library(lubridate)
+library(reshape2)
+
+prices <- read.csv("prices.csv", sep = "|")
+items <- read.csv("items.csv", sep = "|")
+train <- read.csv("train.csv", sep = "|")
+
+# separate dates (123 days, last date: 01/31/18)
+train$year <- year(ymd(train$date))
+train$month <- month(ymd(train$date))
+train$day <- day(ymd(train$date))
+train$weekday <- weekdays(ymd(train$date))
+
+
+
+# chosen: Color
+color <- data.frame(table(items$color))
+colnames(color) <- c("color", "frequency")
+
+# there are 4 major colors: black, blue, white and red
+# 4 submajor: grey, green, gold and orange
+ggplot(items, aes(color)) + geom_bar()
+color[order(color$frequency, decreasing = TRUE),]
+
+
+
+# merge datasets:
+detailed_train <- merge(items, train, by = c("pid", "size"))
+
+# extract_per_month <- function(m) {
+#    m10 <- detailed_train[detailed_train$month == m,]
+#    return(data.frame(sold_oct = tapply(m10$units, m10$color, sum)))
+# }
+
+m10 <- detailed_train[detailed_train$month == 10,]
+s10 <- data.frame(sales_oct = tapply(m10$units, m10$color, sum))
+m11 <- detailed_train[detailed_train$month == 11,]
+s11 <- data.frame(sales_nov = tapply(m11$units, m11$color, sum))
+m12 <- detailed_train[detailed_train$month == 12,]
+s12 <- data.frame(sales_dec = tapply(m12$units, m12$color, sum))
+m01 <- detailed_train[detailed_train$month == 01,]
+s01 <- data.frame(sales_jan = tapply(m01$units, m01$color, sum))
+sales_by_col <- cbind(s10, s11, s12, s01)
+
+# sold units by color per month
+sales_by_col[order(sales_by_col$sales_oct, decreasing = TRUE),]
+ 
+# line graph of sold by color per month
+ggplot(sales_by_col, aes(x = ))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Items sold by color in different months 
+# boxplots did not work because of the small values and outliers
+# ggplot(m10, aes(x = color, y = units)) + geom_boxplot() 
+ggplot(m10, aes(x = "", fill = color)) + geom_bar() + coord_polar("y")
+
+
+# cluster products including color
+
+
+# correlation with the 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
