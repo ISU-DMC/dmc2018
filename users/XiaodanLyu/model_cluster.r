7767a45ba4deb89e7c128f8b98e8a7b14e7bdefe
XiaodanLyu <wuhandaxue2013@gmail.com>
2018-05-08 19:49:07 -0500

    update
    unzip raw data first

--- users/XiaodanLyu/model_cluster.r
+++ users/XiaodanLyu/model_cluster.r
@@ -1,5 +1,7 @@
 setwd("~/Google Drive/dmc2018/users/XiaodanLyu")
 
+library(tidyverse)
+library(lubridate)
 ## read raw datasets
 train <- read.csv("../../data/raw_data/train.csv", sep = "|", stringsAsFactors = F)
 prices <- read.csv("../../data/raw_data/prices.csv", sep = "|", stringsAsFactors = F)
@@ -25,22 +27,32 @@ prices_long %>% summary()
 trend.google <- read.csv("feature_engineering/google_trend_Yuchen.txt", header = T, sep = "|")
 trend.google %>% mutate(date = mdy(date)) -> trend.google
 
+train <- train %>% mutate(date = ymd(date), size = replace(size, size == "", "42"))
+prices_long <- prices_long %>% mutate(size = replace(size, size == "", "42"))
+items <- items %>% mutate(pid = as.numeric(pid))
+
 ## join three datasets
-alldata <- train %>% mutate(date = ymd(date)) %>%
+alldata <- train %>%
   full_join(prices_long, by = c("pid", "size", "date")) %>% 
-  full_join(items %>% mutate(pid = as.numeric(pid)), by = c("pid", "size")) %>%
+  full_join(items, by = c("pid", "size")) %>%
   full_join(trend.google, by = "date") %>%
-  filter(date>=releaseDate) %>% ## remove data before releasedate
+  filter(ymd(date)>=ymd(releaseDate)) %>% ## remove data before releasedate
   mutate(units = replace(units, is.na(units) & date < ymd("2018-02-01"), 0),
          discount = (rrp-price)/rrp*100)
+
+alldata %>% select(pid, size) %>% unique %>% dim
 
 any(is.na(alldata %>% select(-units)))
 
 ## cluster results
 cluster_hf <- read.table("cluster_distance/cluster_hengfang.txt", sep = "|", header = T)
+cluster_hf <- cluster_hf %>% mutate(size = replace(size, size == "", "42"))
 cluster_hf %>% glimpse
+table(cluster_hf$group5)
 
-data5.4 <- alldata %>%
-  left_join(cluster_hf %>% filter(group5 == 4), by = c("pid", "size")) %>%
+## change group5 indicator
+data5 <- cluster_hf %>% filter(group5 == 1) %>%
+  left_join(alldata, by = c("pid", "size")) %>%
   select(-group4, -group5) %>% filter(date < ymd("2018-02-01"))
-
+## check number of products in selected group
+data5 %>% select(pid, size) %>% unique %>% dim
