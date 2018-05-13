2da4b79913adb3154958d2232ff43ae3281dfd2a
Shan Yu <syu@iastate.edu>
2018-05-13 12:04:08 -0500

    modify the waiting time for xgboost

--- users/ShanYu3393/Waiting_TimeXg.R
+++ users/ShanYu3393/Waiting_TimeXg.R
@@ -13,9 +13,9 @@ Total_W$startdate=Total_W0$startdate[Total_W0$Rcr==0]
 Total_W$pid=Total_W0$pid[Total_W0$Rcr==0]
 Total_W$size=Total_W0$size[Total_W0$Rcr==0]
 
-## --------------------- split 2018-01
+## --------------------- split 2018-01-03
 # train
-Train=Total_W[Total_W$startdate <= ymd('2017-12-31'),] %>% select(-startdate)
+Train=Total_W[Total_W$startdate <= ymd('2018-01-03'),] %>% select(-startdate)
 Weight=Train %>% group_by(pid,size) %>% tally()
 Weight$n=1/Weight$n
 Train=left_join(Train,Weight,by=c('pid','size'))
@@ -27,15 +27,23 @@ Train$n=NULL
 Train=data.matrix(Train)
 
 # test
-Test=Total_W[Total_W$startdate > ymd('2017-12-31'),]
+Test=Total_W[Total_W$startdate > ymd('2018-01-03'),]
 Test$startdate=NULL
 Test$size=NULL
-Test$startdate=NULL
 Test=data.matrix(Test)
 
-fitted = xgboost(data=Train, label=Train[,1], max_depth=40, eval_metric="mae",
+fitted = xgboost(data=Train[,-1], label=Train[,1], max_depth=40, eval_metric="mae",
                  weight=Weight_train,
-        subsample=0.6, eta=0.3, objective="count:poisson", nrounds = 160)
+        subsample=0.6, eta=0.3, objective="count:poisson",  nrounds = 100)
 
 Pred=predict(fitted, Test[,-1])
-Pmae=mean(abs(Test[,1]-Pred))
+Pmae=sqrt(sum(abs(Test[,1]-Pred)))
+
+sqrt(sum(abs(Test[,1]-16)))
+
+hist(Pred)
+table(Test[,1]==1)
+
+fitted = train(data=Train, label=Train[,1], max_depth=40, eval_metric="mae",
+                 weight=Weight_train,
+                 subsample=0.6, eta=0.3, objective="reg:linear", nrounds = 10)
